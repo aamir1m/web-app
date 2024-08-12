923beb8ab2e95120bbb1209fbbf05e8f8cf59ebb
aamir1m <aamirmogra1@gmail.com>
2024-08-12 15:45:35 +0100

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Alert from "./components/Alert";
+import ListGroup from "./components/listGroup";
+
+function App() {
+  return (
+    <div>
+      <Alert>Hello World</Alert>
+    </div>
+  );
+}
+
+export default App;
